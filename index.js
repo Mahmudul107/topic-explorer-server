f894a84e34aa449438c1c2259d6f11b57a65e880
Mahmudul107 <mahmudulislam378@gmail.com>
2023-06-17 21:06:45 +0600

    created category and topics collection and get the data from server to client

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
 // Middleware configuration
 app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.py1cfi4.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const categoryCollection = client.db('topicDB').collection('category');
+    const topicsCollection = client.db('topicDB').collection('topics');
+
+
+    // category collection
+    app.get('/category', async (req, res) => {
+        const result = await categoryCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+    // topics collection
+    app.get('/topics', async (req, res) => {
+        const result = await topicsCollection.find().toArray();
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 
